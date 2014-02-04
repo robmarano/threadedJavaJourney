ff24c8b4d0c4f10417f522c0f86dd4237a8bd718
Rob Marano <rob@thehackerati.com>
2014-02-04 18:46:44 -0500

    Adding producer-consumer examples.

---
+++ src/main/java/edu/cooper/ece465/ProducerConsumerExample.java
@@ -0,0 +1,9 @@
+package edu.cooper.ece465;
+
+public class ProducerConsumerExample {
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
