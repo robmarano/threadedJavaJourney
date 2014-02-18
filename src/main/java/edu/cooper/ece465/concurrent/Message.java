1a17f6f6d80949528587922730d34ef90e7ad2f7
Rob Marano <rob@thehackerati.com>
2014-02-17 23:19:33 -0500

    Added edu.cooper.ece465.concurrent package to demonstrate how to use BlockingQueue<> for producer-consumer processing using threads.

---
+++ src/main/java/edu/cooper/ece465/concurrent/Message.java
@@ -0,0 +1,14 @@
+package edu.cooper.ece465.concurrent;
+
+public class Message {
+    private String msg;
+    
+    public Message(String str){
+        this.msg=str;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+}
